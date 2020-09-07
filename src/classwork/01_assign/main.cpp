27ccab5aa938585dc116ed928512daaa064a2277
unif2 <velocci@utexas.edu>
2020-09-07 00:04:14 -0500

    indenting the code

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -7,13 +7,13 @@ using std::cin;
 
 int main() 
 {
-int num;
-cin>>num; //User inputs a number
-int result = multiply_numbers(num); //Apply the function to that number
-cout<<result<<"\n"; //Display the result
-int num1 = 4;
-result = multiply_numbers(num1); //Apply the function to the number 4
-cout<<result<<"\n"; //Display the result
-return 0; //optional only in main function
+    int num;
+    cin>>num; //User inputs a number
+    int result = multiply_numbers(num); //Apply the function to that number
+    cout<<result<<"\n"; //Display the result
+    int num1 = 4;
+    result = multiply_numbers(num1); //Apply the function to the number 4
+    cout<<result<<"\n"; //Display the result
+    return 0; //optional only in main function
 }
 
