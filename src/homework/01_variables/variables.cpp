ea0cdcee4de5e67ff4f1b49280d005f67d0cf88c
unif2 <velocci@utexas.edu>
2020-09-12 00:21:41 -0500

    Added my homework due Sept 18.

--- src/homework/01_variables/variables.cpp
+++ src/homework/01_variables/variables.cpp
@@ -1,14 +1,16 @@
 #include "variables.h"
-
-//example
-int add_numbers(int num1, int num2)
-{
-	return num1 + num2;
-}
-
 //write code for multiply_numbers function here
 //function returns product of num1 and num2
 
+double get_sales_tax_amount(double meal_amount)
+{
+	const double tax_rate = 0.0675;
+	return meal_amount * tax_rate;
+}
 
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+	return meal_amount * tip_rate;
+}
 
 
