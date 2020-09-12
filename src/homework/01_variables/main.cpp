5d8943dff986335865f585453639dafd87b5465a
unif2 <velocci@utexas.edu>
2020-09-12 00:44:07 -0500

    Adding code for rounding to nearest cent.

--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -1,5 +1,6 @@
 ﻿//write include statements
 #include<iostream>
+#include<cmath>
 #include "variables.h"
 
 //write namespace using statement for cout
@@ -15,9 +16,11 @@ int main()
 	cout<<"Enter meal amount: ";
 	cin>>meal_amount;
 	tax_amount = get_sales_tax_amount(meal_amount);
+	tax_amount = floor(tax_amount / 0.01 + 0.5) * 0.01;
 	cout<<"Enter tip rate: ";
 	cin>>tip_rate;
 	tip_amount = get_tip_amount(meal_amount, tip_rate);
+	tip_amount = floor(tip_amount / 0.01 + 0.5) * 0.01;
 	total = tip_amount + tax_amount + meal_amount;
 	cout<<"\nMeal Amount:    "<<meal_amount<<"\n";
 	cout<<"Sales Tax:      "<<tax_amount<<"\n";
