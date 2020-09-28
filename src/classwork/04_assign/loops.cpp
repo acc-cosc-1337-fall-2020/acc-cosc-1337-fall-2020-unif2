4a153f3d31aa52158f1a5b623831a92afc868267
unif2 <velocci@utexas.edu>
2020-09-27 21:51:08 -0500

    Submitting homework due Oct 2.

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -1,1 +1,14 @@
 //cpp
+#include "loops.h"
+
+int factorial(int num)
+{
+    int prod = 1;
+
+    for(int cnt = 1; cnt <= num; cnt++)
+    {
+        prod = prod * cnt;
+    }
+
+    return prod;
+}
--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -1,7 +1,8 @@
 //write includes statements
-
+#include<iostream>
+#include "loops.h"
 //write using statements for cin and cout
-
+using std::cout; using std::cin;
 
 /*
 Use a do while loop to prompt the user for 
@@ -10,6 +11,25 @@ factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
 {
-
+	char choice;
+	do
+	{
+		auto number{1.0};
+		int prod;
+		cout<<"Enter an integer between 1 and 50: ";
+		cin>>number;
+		while (number < 1 || number > 50)
+		{
+			cout<<"\nThe integer must be between 1 and 50.  Try again.\n";
+			cout<<"Enter an integer between 1 and 50: ";
+			cin>>number;
+		}
+		int num = number;
+		prod = factorial(num);
+		cout<<"\n"<<num<<"! = "<<prod;
+		cout<<"\nEnter y to continue: ";
+		cin>>choice;
+	} while (choice == 'y' || choice == 'Y');
+	
 	return 0;
 }
