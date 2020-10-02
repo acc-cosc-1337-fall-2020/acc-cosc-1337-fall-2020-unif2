d51087e33a17191c8f02414d5ac46d3e04a9cfae
unif2 <velocci@utexas.edu>
2020-10-01 22:34:38 -0500

    classwork from 10/1.

--- src/examples/04_module/05_value_and_reference_params/value_ref.h
+++ src/examples/04_module/05_value_and_reference_params/value_ref.h
@@ -1,8 +1,12 @@
 int get_area(int height=20, int width=10);
+
+void sample_static_var();
 /*
 Write function prototype for pass_by_val_and_ref with
 a value int and ref int parameter.
 */
+void pass_by_val_and_ref(int num1);
+
 void pass_by_val_and_ref(int num1, int& num2);
 
 
@@ -11,5 +15,3 @@ Write function prototype for pass_by_const_ref with
 const int reference parameter
 */
 void pass_by_const_ref(const int& num);
-
-void sample_static_var();
--- src/examples/04_module/06_vectors/vec.cpp
+++ src/examples/04_module/06_vectors/vec.cpp
@@ -1,21 +1,94 @@
 #include "for_ranged.h"
 #include<iostream>
 #include<vector>
-
+#include "vec.h"
 using std::vector;
 
+void loop_string_w_index(std::string str)
+{
+    for(std::size_t i=0; i < str.size(); ++i)
+    {
+        str[i] = 'z';
+    }
+}
+
+void loop_string_w_index_ref(std::string& str)
+{
+    for(std::size_t i=0; i < str.size(); ++i)
+    {
+        str[i] = 'z';
+    }
+}
+
+void loop_string_for_ranged(std::string str)
+{
+    for(auto c: str)
+    {
+        c = 'y';
+    }
+}
+
+void loop_string_for_ranged_ref(std::string& str)
+{
+    for(auto c: str)
+    {
+        c = 'y';
+    }
+}
+
+void loop_string_for_ranged_ref_fr_ref(std::string& str)
+{
+    for(auto& c: str)
+    {
+        c = 'y';
+    }
+}
 /*
 Function loop_vector_w_index 
 The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows: 9 10 99 5  67
 
 @param none
-
-
 */
+void loop_vector_w_index(std::vector<int> nums)
+{
+    for(std::size_t i=0; i<nums.size(); ++i)
+    {
+        nums[i] = 0;
+    }
+}
 
+void loop_vector_w_index_ref(std::vector<int>& nums)
+{
+    for(std::size_t i=0; i<nums.size(); ++i)
+    {
+        nums[i] = 0;
+    }
+}
 
+void loop_vector_for_ranged(std::vector<int> nums)
+{
+    for(auto n: nums)
+    {
+        n = 0;
+    }
+}
 
+void loop_vector_for_ranged_ref(std::vector<int>& nums)
+{
+    for(auto n: nums) //value target variable
+    {
+        n = 0;
+    }
+}
+
+void loop_vector_for_ranged_ref_fr_ref(std::vector<int>& nums)
+{
+    for(auto& n: nums) //reference target variable
+    {
+        n = 0;
+    }
+}
 /*
 Function loop_vector_w_index_val 
 Displays the incoming parameter vector numbers to screen using an indexed for loop.
