9b0c93442daa434b8c5124941d3efd443c566a0b
unif2 <velocci@utexas.edu>
2020-10-28 00:47:01 -0500

    Homework due 10/30 (in progress).

--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -63,3 +63,74 @@ void TicTacToe::clear_board()
 {
     pegs = {" "," "," "," "," "," "," "," "," "};
 }
+
+bool TicTacToe::game_over()
+{   //Even if winner wins and fills the board on the last move, this statement will be true.
+    if(TicTacToe::check_column_win() || TicTacToe::check_row_win() || TicTacToe::check_diagonal_win())
+    {
+        set_winner();
+        return true;
+    }
+    //Previous statement here, so if board is full, then the game ends in a tie.
+    else if(TicTacToe::check_board_full())
+    {
+        winner = "C";
+        return true;
+    }
+    //Nobody has won and the board is not full, so game is not over yet.
+    else
+    {
+        return false;
+    }
+    
+}
+
+bool TicTacToe::check_column_win()
+{   //0,3,6 or 1,4,7 or 2,5,8
+    for(std::size_t i=0; i < pegs.size()/3; ++i)
+    {
+        if((pegs[i] == pegs[i+3]) && (pegs[i] == pegs[i+6]) && (pegs[i] != " "))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool TicTacToe::check_row_win()
+{   //1,2,3 or 4,5,6 or 7,8,9
+    for(std::size_t i=0; i < pegs.size()/3; ++i)
+    {
+        if((pegs[3*i] == pegs[3*i+1]) && (pegs[3*i] == pegs[3*i+2] && (pegs[3*i] != " ")))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool TicTacToe::check_diagonal_win()
+{   //0,4,8 or 2,4,6
+    if((pegs[0] == pegs[4]) && (pegs[0] == pegs[8]) && (pegs[0] != " "))
+    {
+        return true;
+    }
+
+    else if((pegs[2] == pegs[4]) && (pegs[2] == pegs[6]) && (pegs[2] != " "))
+    {
+        return true;
+    }
+    return false;
+}
+
+void TicTacToe::set_winner()
+{   //O made the winning move, so player is set to X. Otherwise, X made the winning move.
+    if(player == "X")
+    {
+        winner = "O";
+    }
+    else
+    {
+        winner = "X";
+    }
+}
--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -11,18 +11,22 @@ using std::vector;
 class TicTacToe
 {
 public:
-    bool game_over(){return check_board_full();}
+    bool game_over();
     void start_game(string first_player);
     void mark_board(int position);
     string get_player()const{return player;}
     void display_board()const;
-
+    string get_winner(){return winner;}
 private:
     void set_next_player();
     bool check_board_full();
     void clear_board();
     string player;
     vector<string> pegs{" "," "," "," "," "," "," "," "," "};
-
+    bool check_column_win();
+    bool check_row_win();
+    bool check_diagonal_win();
+    void set_winner();
+    string winner;
 };
 #endif
