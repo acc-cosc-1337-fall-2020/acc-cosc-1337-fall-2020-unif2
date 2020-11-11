7beb763fdc85d002217355bfd30bbbde32711c19
unif2 <velocci@utexas.edu>
2020-11-10 22:08:15 -0600

    Classwork from 11/10.

--- src/classwork/06_assign/main.cpp
+++ src/classwork/06_assign/main.cpp
@@ -10,8 +10,35 @@ Width	Height		Area
 Iterate the vector and display the Area for each Rectangle on one line and the total area for the
 3 rectangles.
 */
+#include<iostream>
+#include<fstream>
+#include<string>
 int main()
 {
+  std::ofstream out_file;
+  std::cout<<"Open file: \n";
+  out_file.open("names.dat");
+
+  out_file<<"joe"<<std::endl;
+  out_file<<"may"<<std::endl;
+  out_file<<"john"<<std::endl;
+
+  std::cout<<"Close file\n";
+  
+  //READ FILE
+
+  std::ifstream in_file;
+  std::cout<<"\n open file\n\n";
+  in_file.open("names.dat");
+
+  std::string name;
+  std::cout<<"Names\n";
+  while(in_file>>name)
+  {
+    std::cout<<name<<"\n";
+  }
+
+  in_file.close();
 	
 	return 0;
 }
